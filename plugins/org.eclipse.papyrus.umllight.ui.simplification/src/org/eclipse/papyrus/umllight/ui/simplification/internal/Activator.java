9fbb92e21fd82b023f20224558bf7435b1f6b698
Tobias Ortmayr <tortmayr@eclipsesource.com>
2018-12-21 11:05:14 +0100

    Fix #68 Disable UML Metamodel context

--- plugins/org.eclipse.papyrus.umllight.ui.simplification/src/org/eclipse/papyrus/umllight/ui/simplification/internal/Activator.java
+++ plugins/org.eclipse.papyrus.umllight.ui.simplification/src/org/eclipse/papyrus/umllight/ui/simplification/internal/Activator.java
@@ -12,7 +12,12 @@
 
 package org.eclipse.papyrus.umllight.ui.simplification.internal;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.papyrus.umllight.ui.simplification.internal.ContextConfigurator.Context;
+import org.eclipse.ui.IStartup;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.ui.progress.UIJob;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -35,13 +40,13 @@ public class Activator extends AbstractUIPlugin {
 
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-
 		plugin = this;
+		configureCreationMenus();
+		ContextConfigurator.disableContext(Context.UML);
 	}
 
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
-
 		super.stop(context);
 	}
 
@@ -54,4 +59,30 @@ public class Activator extends AbstractUIPlugin {
 		return plugin;
 	}
 
+	private void configureCreationMenus() {
+		// Do this on the UI thread because that's the context in which the creation
+		// menu registry is generally accessed
+		new UIJob("Initializing creation menus") {
+
+			@Override
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				return CreationMenuCleaner.clean();
+			}
+		}.schedule();
+	}
+	
+	// 
+	  // Nested types 
+	  // 
+	 
+	  /** 
+	   * An early startup hook that cleans the creation menu registry disables unwanted contexts. 
+	   */ 
+	  public static class Startup implements IStartup { 
+	    @Override 
+	    public void earlyStartup() { 
+	      // Nothing really to do but kick the activator 
+	      Activator.getDefault(); 
+	    } 
+	  }
 }
